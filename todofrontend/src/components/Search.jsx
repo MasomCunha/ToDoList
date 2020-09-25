c4da5d63b71e15f63b81393c2925055799b49fe0
Mmc <manuel.cunha@optimizer.pt>
2020-09-25 11:12:03 +0100

    search add clear

--- todofrontend/src/components/Search.jsx
+++ todofrontend/src/components/Search.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { fetchSearchTodos } from '../store/todoActions'
+import { fetchSearchTodos, fetchTodos } from '../store/todoActions'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-const Search = ({ fetchSearchTodos, userID, userKey }) => {
+const Search = ({ fetchSearchTodos, userID, userKey, fetchTodos }) => {
 
     const [inputSearch, setInputSearch] = useState("")
 
@@ -11,11 +11,24 @@ const Search = ({ fetchSearchTodos, userID, userKey }) => {
         setInputSearch(e.target.value)
     }
 
+    const handleClear = () => {
+        setInputSearch("")
+        fetchData(userKey)
+    }
+
+    const fetchData = () => {
+        if(inputSearch === "") {
+            fetchTodos(userKey)
+        } else {
+            fetchSearchTodos(userKey, inputSearch, userID)
+        }
+    }
 
     return (
         <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ marginLeft: "40%" }}>
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={handleChange} style={{width: "60%"}} />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={ () => fetchSearchTodos(userKey, inputSearch, userID)}>Search</button>
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={handleChange} value={inputSearch} style={{width: "60%"}} />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={ fetchData }>Search</button>
+            <button className="btn btn-outline-dark my-2 my-sm-0" type="submit" onClick={ handleClear }>Clear</button>
         </div>
     )
 
@@ -30,7 +43,7 @@ const mapStateToProps = (state) => (
 )
 
 const mapDispatchToProps = dispatch => bindActionCreators(
-    { fetchSearchTodos }, dispatch
+    { fetchSearchTodos, fetchTodos }, dispatch
 )
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search)
